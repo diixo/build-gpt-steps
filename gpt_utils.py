26cffcaf03a25395a54db4ccb7c7de8a658f9c6c
diixo <anatolii.podoba@gmail.com>
2025-09-11 19:40:06 +0300

    gpt_utils.py: updated

--- gpt_utils.py
+++ gpt_utils.py
@@ -3,16 +3,16 @@ import torch
 from torch.nn import functional as F
 
 
-def generate_new_text(prompt: str, model, enc, device, device_type, max_length=32):
+def generate_new_text(prompt: str, model, enc, device, device_type, max_new_length=20):
     model.eval()
 
-    inputs = enc.encode(prompt) + [enc.eos_token_id]    # EOS ???
+    inputs = enc.encode(prompt) + [enc.eos_token_id]
     tokens = torch.tensor(inputs, dtype=torch.long).unsqueeze(0) # (1, T)
     xgen = tokens.to(device)
 
-    while xgen.size(1) < max_length:
-        # forward the model to get the logits
-        with torch.no_grad():
+    # forward the model to get the logits
+    with torch.no_grad():
+        for _ in range(max_new_length):
             with torch.autocast(device_type=device_type, dtype=torch.bfloat16):
                 logits, loss = model(xgen) # (B, T, vocab_size)
             # take the logits at the last position
@@ -24,11 +24,11 @@ def generate_new_text(prompt: str, model, enc, device, device_type, max_length=3
             # append to the sequence
             xgen = torch.cat((xgen, xcol), dim=1)
     # return generated text
-    decoded = xgen[0, :max_length].tolist()
+    decoded = xgen[0, :len(inputs)].tolist()
     return enc.decode(decoded[len(inputs):])
 
 
-def generate_new_text_sft(prompt: str, model, enc, device, device_type, max_new_length=16):
+def generate_new_text_sft(prompt: str, model, enc, device, device_type, max_new_length=20):
     model.eval()
 
     inputs = enc.encode(prompt) + [enc.sep_token_id]  # input + SEP
@@ -45,6 +45,5 @@ def generate_new_text_sft(prompt: str, model, enc, device, device_type, max_new_
             if next_id.item() == enc.eos_token_id:
                 break
 
-    sep_len = len(inputs)  # input + SEP
-    decoded = xgen[0, sep_len:].tolist()
+    decoded = xgen[0, len(inputs):].tolist()
     return enc.decode(decoded)
